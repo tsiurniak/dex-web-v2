4b1a84bcba96b79ffc2b385070bc91837a077ba8
Oleksandr Tsiurniak <oleswot@gmail.com>
2022-06-07 21:16:08 +0300

    small fix

--- pages/api/abi.js
+++ pages/api/abi.js
@@ -57,38 +57,12 @@ export default [
     },
     {
       "inputs": [],
-      "name": "minter",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
       "name": "name",
       "outputs": [
         {
           "internalType": "string",
           "name": "",
           "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "tokenPrice",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
         }
       ],
       "stateMutability": "view",
